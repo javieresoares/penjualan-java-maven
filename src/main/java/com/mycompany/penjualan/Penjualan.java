e5f966580038c5d25f1e77da1740298b7f80e6a6
javieresoares <113227657+javieresoares@users.noreply.github.com>
2024-10-16 18:00:02 +0700

    Initial commit

---
+++ src/main/java/com/mycompany/penjualan/Penjualan.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.penjualan;
+
+/**
+ *
+ * @author ACER - Gilardinho A11.2022.14732
+ */
+
+public class Penjualan {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
